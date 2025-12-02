f2869b80c80e3ac413bec72b1ff443262bbf5271
sumitdwivedi01 <sumitdwivedi056@gmail.com>
2025-12-02 23:46:49 +0530

    combination_sum_2 done

--- 05_Recursion/combination_sum_2.cpp
+++ 05_Recursion/combination_sum_2.cpp
@@ -1,31 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-void get_sum(vector<int>& nums , int target,int i , vector<int> &crr , set<vector<int>> & result){
-    if(i>=nums.size()){
-        if(target==0){
-            sort(crr.begin(),crr.end());
-            result.insert(crr);
-        }
+ //time complexity : O(2^n * K)
+ //space complexity:O()
+void get_sum(vector<int>& nums , int target,int i , vector<int> &crr , vector<vector<int>> & result){
+    if(target==0){
+        result.push_back(crr);
         return;
     }
-    if(target>=nums[i]){
-        int temp=nums[i];
-        crr.push_back(temp);
-        target-=temp;
-        get_sum(nums,target,i+1,crr,result);
-        target+=temp;
+    for(int j=i;j<nums.size();j++){
+        if(j>i && nums[j]==nums[j-1])continue;
+        if(nums[j]>target)break;
+
+        crr.push_back(nums[j]);
+        get_sum(nums,target-nums[j],j+1,crr,result);
+
         crr.pop_back();
     }
-    get_sum(nums,target,i+1,crr,result);
 
 }
+// void get_sum(vector<int>& nums , int target,int i , vector<int> &crr , vector<vector<int>> & result){
+//     if(i>=nums.size()){
+//         if(target==0){
+//             for(auto it:result){
+//                 if(it==crr)return;
+//             }
+//             result.push_back(crr);
+//         }
+//         return;
+//     }
+//     if(target>=nums[i]){
+//         int temp=nums[i];
+//         crr.push_back(temp);
+//         target-=temp;
+//         get_sum(nums,target,i+1,crr,result);
+//         target+=temp;
+//         crr.pop_back();
+//     }
+//     get_sum(nums,target,i+1,crr,result);
+
+// }
 void solve(){
-    vector<int>nums={2, 1, 2, 7, 6, 1, 5};
+    vector<int>nums={1,2,1,2,4,3};
     int n=nums.size();
-    int target=8;
+    int target=7;
+    sort(nums.begin(),nums.end());
     vector<int> current;
-    set<vector<int>> result;
+    vector<vector<int>> result;
     get_sum(nums,target,0,current,result);
     for(auto it : result){
         for(auto s: it){
