00a8e916dbe587373f5ec62eb0bca44dc04a5fdd
sumitdwivedi01 <sumitdwivedi056@gmail.com>
2025-12-02 16:14:37 +0530

    added gitignore

---
+++ 05_Recursion/combination_sum_2.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void get_sum(vector<int>& nums , int target,int i , vector<int> &crr , vector<vector<int>> & result){
+    if(i>=nums.size()){
+        if(target==0){
+            result.push_back(crr);
+        }
+        return;
+    }
+    if(target>=nums[i]){
+        int temp=nums[i];
+        crr.push_back(temp);
+        target-=temp;
+        get_sum(nums,target,i+1,crr,result);
+        target+=temp;
+        crr.pop_back();
+    }
+    get_sum(nums,target,i+1,crr,result);
+
+}
+void solve(){
+    vector<int>nums={2, 1, 2, 7, 6, 1, 5};
+    int n=nums.size();
+    int target=8;
+    vector<int> current;
+    vector<vector<int>> result;
+    get_sum(nums,target,0,current,result);
+    for(auto it : result){
+        for(auto s: it){
+            cout<<s<<" ";
+        }
+        cout<<endl;
+    }
+
+    
+}
+
+int main(){
+    
+    int t =1;
+
+    // cin>>t;
+
+
+    while(t--){
+
+        solve();
+    }
+
+
+    return 0;
+}
